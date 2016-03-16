3bd0cb9ac940624cc1f22bf201ac7815bb377279
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-03-16 13:37:54 -0500

    set up postgres database

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 'use strict';
-const express = require('express');
-const app = express();
-const routes = require('./routes/index.js');
-const PORT = process.env.PORT || 3000;
-const pg = require('pg');
 
+const express   = require('express');
+const app       = express();
+
+const pg        = require('pg');
 pg.defaults.ssl = false;
 
-app.use(express.static('www'));
+const Sequelize = require('sequelize');
 
+const routes    = require('./routes/index.js');
+
+const PORT      = process.env.PORT || 3000;
+
+//enviroment variable set on heroku or use my localhost for dev.
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/gifdailydb';
+
+const sequelize = new Sequelize(DATABASE_URL);
+
+
+app.use(express.static('www'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
@@ -19,8 +29,8 @@ app.all('*', function(req, res, next) {
 
 app.use(routes);
 
-//connecting to heroku postgres database
-pg.connect(process.env.DATABASE_URL, function(err, client) {
+//connecting to postgres database
+pg.connect(DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
