8c27ff4a232e02bc0ef9a1342a6c78c61930a3fd
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-03-16 11:08:32 -0500

    removed db folder, and added comments on server.js

--- server.js
+++ server.js
@@ -19,15 +19,11 @@ app.all('*', function(req, res, next) {
 
 app.use(routes);
 
+//connecting to heroku postgres database
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
-  //client
-    //.query('SELECT table_schema,table_name FROM information_schema.tables;')
-    //.on('row', function(row) {
-      //console.log(JSON.stringify(row));
-    //});
 });
 
 app.listen(PORT, function () {
